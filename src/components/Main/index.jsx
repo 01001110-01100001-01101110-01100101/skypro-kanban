0a4ba54fecca60b87eebff5f86265fe56475e3cf
01001110-01100001-01101110-01100101 <nane.akopyan.2004@mail.ru>
2024-06-05 00:25:25 +0300

    hw1

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,18 +1,18 @@
 import { Column } from "../Column";
 export const Main = () => {
  return (
-  <main className="main">
-   <div className="container">
-    <div className="main__block">
-     <div className="main__content">
-      <Column title="Без статуса" />
-      <Column title="Нужно сделать" />
-      <Column title="В работе" />
-      <Column title="Тестирование" />
-      <Column title="Готово" />
-     </div>
-    </div>
-   </div>
-  </main>
+	<main className="main">
+		<div className="container">
+			<div className="main__block">
+				<div className="main__content">
+					<Column title="Без статуса" />
+					<Column title="Нужно сделать" />
+					<Column title="В работе" />
+					<Column title="Тестирование" />
+					<Column title="Готово" />
+				</div>
+			</div>
+		</div>
+	</main>
  );
 };
