625dde4d504ee721a035507a77b72aad3a8a9a1d
01001110-01100001-01101110-01100101 <nane.akopyan.2004@mail.ru>
2024-07-10 22:29:31 +0300

    hw3

--- src/components/Main/main.styled.js
+++ src/components/Main/main.styled.js
@@ -2,7 +2,8 @@ import styled from "styled-components";
 
 export const Main = styled.main`
     width: 100%;
-    background-color: ${({theme}) => theme.body};
+    height: 100vh;
+    background-color: ${({ theme }) => theme.body};
 `;
 
 export const MainBlock = styled.div`
